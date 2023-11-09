d16d0907608684306400d15dac5edf5871739b8e
Axel Mc Allister <axelma@ext.inditex.com>
2023-11-09 11:17:00 +0100

    1

--- src/main/java/org/example/Rental.java
+++ src/main/java/org/example/Rental.java
@@ -7,6 +7,34 @@ public class Rental
     this.daysRented = daysRented;
   }
 
+  int getFrequentRenterPoints() {
+    int frequentRenterPoints = 1;
+    if (getMovie ().getPriceCode () == Movie.NEW_RELEASE
+        && getDaysRented () > 1)
+      frequentRenterPoints++;
+    return frequentRenterPoints;
+  }
+
+  double calculateAmount() {
+    double thisAmount = 0;
+    switch (getMovie ().getPriceCode ()) {
+      case Movie.REGULAR:
+        thisAmount += 2;
+        if (getDaysRented () > 2)
+          thisAmount += (getDaysRented () - 2) * 1.5;
+        break;
+      case Movie.NEW_RELEASE:
+        thisAmount += getDaysRented () * 3;
+        break;
+      case Movie.CHILDRENS:
+        thisAmount += 1.5;
+        if (getDaysRented () > 3)
+          thisAmount += (getDaysRented () - 3) * 1.5;
+        break;
+    }
+    return thisAmount;
+  }
+
   public int getDaysRented () {
     return daysRented;
   }
